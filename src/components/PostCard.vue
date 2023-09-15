8a0f445ff54815a015c619d4585f7b9c1dae9995
Austin <austinjperry22@gmail.com>
2023-09-15 14:19:05 -0600

    working on likes

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col-11">
+    <div class="col-11 py-2 my-3 bg-white elevation-5">
         <section class="row">
             <div class="col-2">
                 <img :src="post.creator.picture" class="profile-pic">
@@ -9,7 +9,7 @@
                 <h6>{{ post.createdAt }}</h6>
             </div>
             <div class="col-2">
-                <button v-if="post.creatorId == Account.id" class="btn"><i class="mdi mdi-dots-horizontal"></i></button>
+                <button v-if="post.creatorId == account.id" class="btn"><i class="mdi mdi-dots-horizontal"></i></button>
             </div>
         </section>
         <section class="row">
@@ -22,18 +22,35 @@
                 <img :src="post.imgUrl" class="post-img">
             </div>
         </section>
+        <section v-if="account.id" class="row justify-content-end">
+            <div class="col-6 text-end">
+                <h3 class="mb-0">{{ post.likes.length }}<i class="mdi mdi-heart selectable"></i>
+                </h3>
+                <h3 class="mb-0">{{ post.likes.length }}<i class="mdi mdi-heart-outline selectable"></i></h3>
+            </div>
+        </section>
+        <section v-else class="row justify-content-end">
+            <div class="col-6 text-end">
+                <h3 class="mb-0">Likes: {{ post.likes.length }}</h3>
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
-import { Account } from '../models/Account';
+import { computed, onMounted } from 'vue';
 import { Post } from '../models/Post';
+import { AppState } from '../AppState';
+import { logger } from '../utils/Logger';
 
 export default {
     props: { post: { type: Post, required: true } },
     setup(props) {
+
         return {
-            Account
+            account: computed(() => AppState.account),
+
+
         };
     },
 };
--- src/models/Post.js
+++ src/models/Post.js
@@ -5,7 +5,7 @@ export class Post {
         this.creator = data.creator
         this.createdAt = new Date(data.createdAt).toLocaleDateString()
         this.updatedAt = new Date(data.updatedAt).toLocaleDateString()
-        this.likes = data.likes
+        this.likes = data.likeIds || null
         this.imgUrl = data.imgUrl
         this.body = data.body
     }
