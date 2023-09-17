d9c523b63c45ca34d511721b8b050ca456019290
Austin Perry <austinjperry22@gmail.com>
2023-09-17 17:26:34 -0600

    ?

--- src/models/Account.js
+++ src/models/Account.js
@@ -4,6 +4,11 @@ export class Account {
     this.email = data.email
     this.name = data.name
     this.picture = data.picture
+    this.class = data.class
+    this.github = data.github
+    this.linkedin = data.linkedin
+    this.resume = data.resume
+    this.coverImg = data.coverImg
     // TODO add additional properties if needed
   }
 }
--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -1,20 +1,55 @@
 <template>
-  <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
-    <p>{{ account.email }}</p>
-  </div>
+  <section class="row">
+        <div class="col-12 text-center mt-4 cover-img">
+            <img :src="account.picture" class="account-pic">
+            <h2>{{ account.name }}</h2>
+            <section class="row">
+                <div class="col-6">
+                    <h4>email: {{ account.email }}</h4>
+                </div>
+                <div class="col-6">
+                    <h4>class: {{ account.class }}</h4>
+                </div>
+            </section>
+            <section class="row">
+                <div class="col-6">
+                    <h4>github: {{ account.github }}</h4>
+                </div>
+                <div class="col-6">
+                    <h4>linkedin: {{ account.linkedin }}</h4>
+                </div>
+            </section>
+            <section class="row">
+                <div class="col-6">
+                    <h4>resume: {{ account.resume }}</h4>
+                </div>
+                <div class="col-6">
+                    <h4 v-if="account.graduated">Graduate</h4>
+                    <h4 v-else>Hasn't graduated yet</h4>
+                </div>
+            </section>
+            <section class="row">
+                <h5>Bio: {{ account.bio }}</h5>
+            </section>
+        </div>
+  </section>
+  <form @submit.prevent="editAccount" class="row mt-3">
+        <label for="">hello</label>
+  </form>
+  
   <CreatePost/>
 </template>
 
 <script>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { AppState } from '../AppState';
 import CreatePost from '../components/createPost.vue';
 export default {
     setup() {
+        const accountData = ref({})
         return {
-            account: computed(() => AppState.account)
+            account: computed(() => AppState.account),
+            coverImg: computed(()=> `url(${AppState.account.coverImg}`)
         };
     },
     components: { CreatePost }
@@ -22,7 +57,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-img {
-  max-width: 100px;
+.account-pic {
+    height: 8.5em;
+    width: 8.5em;
+    border-radius: 5em;
+}
+
+.cover-img {
+    background-image: v-bind(coverImg);
+    min-height: 50vh;
+    background-position: center;
+    background-size: cover;
 }
 </style>
